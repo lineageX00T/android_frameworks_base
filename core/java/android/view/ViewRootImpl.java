f879b8e2e93f7c9d0585edc734b5152bcb9b3533
strongreasons <abenkenari@gmail.com>
2025-04-13 15:54:05 +0000

    Revert "ViewRootImpl: Fixed memory leak caussed by viewrootImpl objects"
    This reverts commit 5efa62a5727890d5476f0ba04a9d804063d0f9e8.

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -7321,14 +7321,6 @@ public final class ViewRootImpl implements ViewParent,
             mRemoved = true;
             if (mAdded) {
                 dispatchDetachedFromWindow();
-            } else {
-                Log.w(mTag, "add view failed and remove related objects");
-
-                mAccessibilityManager.removeAccessibilityStateChangeListener(
-                        mAccessibilityInteractionConnectionManager);
-                mAccessibilityManager.removeHighTextContrastStateChangeListener(
-                        mHighContrastTextManager);
-                mDisplayManager.unregisterDisplayListener(mDisplayListener);
             }
 
             if (mAdded && !mFirst) {
